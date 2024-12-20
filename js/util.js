f6c5d93a9e954969c6858ca20f03346cad026a33
Dacst12 <misiya3@yandex.ru>
2024-12-20 22:52:17 +0500

    добавит возможность фильтрации изображений

--- js/util.js
+++ js/util.js
@@ -37,4 +37,20 @@ const showAlert = (message) => {
   }, ALERT_DELETE_TIME);
 };
 
-export {isEscapeKey, showAlert};
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {isEscapeKey, showAlert, getRandomInteger, debounce};
